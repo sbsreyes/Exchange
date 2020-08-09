d82c7aeabf2519be569e772a1e825c6365c89525
Sebastian Reyes <sbsreyes.js@gmail.com>
2020-08-09 15:51:27 -0500

    Integracion con la API rest coincap io

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <px-assets-table v-bind:assets="assets" />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable";
+import api from "@/api";
+export default {
+  name: "Home",
+  data() {
+    return {
+      assets: [],
+    };
+  },
+  created() {
+    api.getAssets().then((assets) => (this.assets = assets));
+  },
+  components: {
+    PxAssetsTable,
+  },
+};
+</script>
