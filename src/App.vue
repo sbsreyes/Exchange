6f52aa73702debcfdd27f673da3b0f71787cb94f
Sebastian Reyes <sbsreyes.js@gmail.com>
2020-08-09 15:17:33 -0500

    Se agregan enrutamientos y nuevas funcionalidades por componentes

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome Sebitas to Your Vue.js App" />
-  </div>
+  <main>
+    <px-header />
+
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import "@/assets/css/tailwind.css";
+import PxHeader from "@/components/PxHeader";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    PxHeader,
+  },
 };
 </script>
 
